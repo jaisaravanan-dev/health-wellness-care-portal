60d64f4c364146d4dbecf0df49d60624876b4aa0
Jai <jaisaravanan.cs@gmail.com>
2025-11-08 02:11:29 -0800

    added the patient detaails component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import Register from "./pages/register/Register";
+import PatientDetails from "./pages/patientDetails/PatientDetail";
 // import Dashboard from "./pages/Dashboard";
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
         
         <Route path="/" element={<AuthPage />} />
         <Route path="/signup" element={<Register />} />
+        <Route path="/patient-details" element={<PatientDetails />} />
+        
         {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
  
