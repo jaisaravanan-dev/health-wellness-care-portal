1d9f05d0ab27a0523ada4f0bb6f7a6a9fb21df05
Jai <jaisaravanan.cs@gmail.com>
2025-11-08 02:06:54 -0800

    update the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
+import Register from "./pages/register/Register";
 // import Dashboard from "./pages/Dashboard";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         
         <Route path="/" element={<AuthPage />} />
+        <Route path="/signup" element={<Register />} />
         {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
  
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -20,52 +20,57 @@ const Register = ({ onToggle }) => {
   };
 
   return (
-    <div className="w-full max-w-sm bg-white shadow-lg rounded-2xl p-8">
-      <div className="flex justify-center mb-4">
-        <div className="bg-gray-200 w-24 h-24 flex items-center justify-center rounded-md">
-          <span className="text-gray-500 text-sm">150×150</span>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="w-full max-w-sm bg-white shadow-lg rounded-2xl p-8">
+        <div className="flex justify-center mb-4">
+          <div className="bg-gray-200 w-24 h-24 flex items-center justify-center rounded-md">
+            <span className="text-gray-500 text-sm">150×150</span>
+          </div>
         </div>
-      </div>
-      <h2 className="text-2xl font-semibold text-center text-blue-600 mb-6">
-        Register
-      </h2>
-
-      <form onSubmit={handleSubmit}>
-        <InputFiled
-          name="username"
-          value={form.username}
-          onChange={handleChange}
-          placeholder="Username"
-        />
-        <InputFiled
-          name="email"
-          type="email"
-          value={form.email}
-          onChange={handleChange}
-          placeholder="Email"
-        />
-        <InputFiled
-          name="password"
-          type="password"
-          value={form.password}
-          onChange={handleChange}
-          placeholder="Password"
-        />
-
-        <button
-          type="submit"
-          className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold py-2 rounded-md transition-all"
-        >
+        <h2 className="text-2xl font-semibold text-center text-blue-600 mb-6">
           Register
-        </button>
-      </form>
+        </h2>
 
-      <p
-        className="text-center text-blue-500 text-sm mt-4 cursor-pointer hover:underline"
-        onClick={onToggle}
-      >
-        Already have an account? Login here
-      </p>
+        <form onSubmit={handleSubmit}>
+          <InputFiled
+            name="username"
+            value={form.username}
+            onChange={handleChange}
+            placeholder="Username"
+            label={"UserName:"}
+          />
+          <InputFiled
+            name="email"
+            type="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="Email"
+            label={"Email:"}
+          />
+          <InputFiled
+            name="password"
+            type="password"
+            value={form.password}
+            onChange={handleChange}
+            placeholder="Password"
+            label={"Password:"}
+          />
+
+          <button
+            type="submit"
+            className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold py-2 rounded-md transition-all"
+          >
+            Register
+          </button>
+        </form>
+
+        <p
+          className="text-center text-blue-500 text-sm mt-4 cursor-pointer hover:underline"
+          onClick={onToggle}
+        >
+          Already have an account? Login here
+        </p>
+      </div>
     </div>
   );
 };
